99cb824be0e3e011dbf33b03de4a326ed1e3e68e
Fahad <fahadmahmood1200@gmail.com>
2021-05-19 18:08:36 +0530

    make it better

---
+++ Public/script.js
@@ -0,0 +1,129 @@
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "443",
+});
+
+let myVideioStream;
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideioStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  });
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
+
+// Message Sending
+
+let text = $("input");
+// when press enter send message
+$("html").keydown((e) => {
+  if (e.which == 13 && text.val().length !== 0) {
+    console.log(text.val());
+    socket.emit("message", text.val());
+    text.val("");
+  }
+});
+socket.on("createMessage", (message) => {
+  console.log("createMessage", message);
+  $(".messages").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+  scrollToBottom();
+});
+
+const scrollToBottom = () => {
+  let d = $(".main__chat_window");
+  d.scrollTop(d.prop("scrollHeight"));
+};
+
+// Mute Button
+const muteUnmute = () => {
+  const enabled = myVideioStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideioStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideioStream.getAudioTracks()[0].enabled = true;
+  }
+};
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+// Video Off/On
+const playStop = () => {
+  console.log("object");
+  let enabled = myVideioStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideioStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideioStream.getVideoTracks()[0].enabled = true;
+  }
+};
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
---
+++ plan.md
@@ -0,0 +1,23 @@
+# plan of Action
+
+-> Initialize our NodeJs Project
+-> Initialize our first view
+-> Create a room Id
+-> Add the ability to view our own Video
+-> Add ability to allow other to stream their video
+-> Add styling
+-> Add the abelity to create message
+-> Add mute Button
+-> Add stop video button
+
+<!-- NODE.JS -->
+<!-- Socket.io -->
+<!-- Pear.JS -->
+<!-- WebRTC -->
+<!-- peerJs -->
+
+{npm i express}
+{npm i ejs}
+{npm i uuid}
+{npm i socket.io}
+{npm i peer}
